35344c7e06f7881703b65586fa3a3602892d89f7
Kishan D R <93592071+kishandr15@users.noreply.github.com>
2024-01-11 12:00:34 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -24,23 +24,6 @@ X_pred = clf.predict(X)
 print(X_pred == y)
 
 
-# Datasets: Filename: PlayTennis.csv
-# outlook,temp,humidity,windy,play
-# Sunny,Hot,High,Weak,No
-# Sunny,Hot,High,Strong,No
-# Overcast,Hot,High,Weak,Yes
-# Rain,Mild,High,Weak,Yes
-# Rain,Cool,Normal,Weak,Yes
-# Rain,Cool,Normal,Strong,No
-# Overcast,Cool,Normal,Strong,Yes
-# Sunny,Mild,High,Weak,No
-# Sunny,Cool,Normal,Weak,Yes
-# Rain,Mild,Normal,Weak,Yes
-# Sunny,Mild,Normal,Strong,Yes
-# Overcast,Mild,High,Strong,Yes
-# Overcast,Hot,Normal,Weak,Yes
-# Rain,Mild,High,Strong,No
-
 # Output:
 # 0 True
 # 1 True
